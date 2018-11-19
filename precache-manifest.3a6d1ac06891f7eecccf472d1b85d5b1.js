6ab94095d4094fa6f4255bedabaa784274fa7218
CoderPrans <pranavbhaskar17@gmail.com>
2018-11-19 21:13:02 +0530

    Updates

---
+++ precache-manifest.3a6d1ac06891f7eecccf472d1b85d5b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9708061682f096157d50af083dd60f2",
+    "url": "/static/media/Zomato_company_logo.c9708061.png"
+  },
+  {
+    "revision": "0e30a1f2aa142a93a747a7148561c939",
+    "url": "/static/media/Rolling-1s-200px.0e30a1f2.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "feecb8caf9b92072f39a",
+    "url": "/static/js/main.feecb8ca.chunk.js"
+  },
+  {
+    "revision": "35011d562c0bdf13276a",
+    "url": "/static/js/1.35011d56.chunk.js"
+  },
+  {
+    "revision": "feecb8caf9b92072f39a",
+    "url": "/static/css/main.87c06a36.chunk.css"
+  },
+  {
+    "revision": "62195a23a8a124f9dca0218197eb0f75",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/AnyFood/precache-manifest.2373c8e06e1291060af51fc5476476a8.js"
+  "/precache-manifest.3a6d1ac06891f7eecccf472d1b85d5b1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/AnyFood/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
